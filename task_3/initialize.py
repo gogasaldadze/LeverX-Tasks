65fd48f510ac1b3c8bbeb3cb055055b6803e90f9
Goga Saldadze <saldadzegoga@gmail.com>
2025-08-11 11:21:56 +0400

    updated initializer

--- task_3/initialize.py
+++ task_3/initialize.py
@@ -1,22 +1,27 @@
-from configurator.connection import ConnectionManager
-from configurator.create_database import CreateDataBase
+from core.connection import ConnectionManager
+from core.create_database import CreateDataBase
 from schema import RoomSchema, StudentSchema, IndexManager
 from data_loader import RoomsDataLoader, StudentDataLoader
 from data_inserter import RoomInserter, StudentInserter
+import getpass
+import sys
+from config.db_config import save_config
 
 
-def initializer():
+def initializer(host, user, password, database):
     #  Connect WITHOUT database to create database
-    raw_connection = ConnectionManager().get_connection()
+    raw_connection = ConnectionManager(host, user, password).get_connection()
     print("SQL server connection Established ! ")
 
     # Database Create
-    CreateDataBase(raw_connection, "task_3_db").create()
+    CreateDataBase(raw_connection, database).create()
     raw_connection.close()
 
     #  Connect WITH database to work with tables and data
-    db_connection = ConnectionManager().get_connection_with_db("task_3_db")
-    print("Successfully connected to Database")
+    db_connection = ConnectionManager(host, user, password).get_connection_with_db(
+        database
+    )
+    print(f"Successfully connected to {database}")
 
     #  Create tables
     with RoomSchema(db_connection) as room_schema:
@@ -40,9 +45,8 @@ def initializer():
     with StudentInserter(db_connection) as student_inserter:
         student_inserter.insert(students)
 
+    # save config
+    save_config(host, user, password, database)
+
     #  Close the DB connection
     db_connection.close()
-
-
-if __name__ == "__main__":
-    initializer()
